6dbd4b66610baa9ec791cb8eb1d8d72dc33914a4
yuanpingshun123 <1143684427@qq.com>
2018-11-07 09:53:50 +0800

    搭建后台的测试

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 //引入koa
 import koa from 'koa'
+// import Router from 'koa-router'
+// import bodyparser from 'koa-bodyparser'
 // 实例化koa
 const app = new koa()
-
+// const router = new Router()
 //生成服务器
+
+
+// app.use(bodyparser())
+// app.use(router.routes())
+
 app.listen(3000,()=>{
-    console.log("node is ok")
+    console.log("node is ok asd")
 })
